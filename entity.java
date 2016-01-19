ada750de2f854f34ccc012257bd7ff289e8ef374
malavan12 <malavan14@hotmail.com>
2016-01-19 15:03:39 -0500

    Update entity.java

--- entity.java
+++ entity.java
@@ -4,16 +4,36 @@ class entity
     int positiony;
     boolean passable;
     int slow;
-    public entity (character player)
+    enemy Enemy;
+    npc NPC;
+    character CHAR;
+    char drawData;
+    int colourData;
+    
+    public entity (String spawnInfo)
     {
-        positionx = player.posx;
-        positiony = player.posy;
-        passable = false;
-    }
-
-
-    public entity (enemy ene)
-    {
+        //"Type;chunkx;chunky;posx;poy;drawTile;drawColour"
+        String [] data = spawnInfo.split(';');
+        if (data[0].equalsIgnoreCase("Enemy"))
+        {
+            NPC = null;
+            CHAR =null;
+            try {
+            Enemy = new enemy (data[5].charAt(0),data[6]);    
+            positionx= Integer.parseInt(data[3]);
+            positiony= Integer.parseInt(data[4]);
+            
+            
+            } catch (Exception e){}
+        }
+        if (data[0].equalsIgnoreCase("NPC"))
+        {
+            
+        }
+        if (data[0].equalsIgnoreCase("Character"))
+        {
+            
+        }
         positionx = ene.posx;
         positiony = ene.posy;
         passable = false;
@@ -27,11 +47,11 @@ class entity
         passable = tera.pass;
     }
     
-    public static void updatePos(int deltaX,int deltaY)
+   /* public static void updatePos(int deltaX,int deltaY)
     {
         positionx+=deltaX;
         positiony+=deltaY;
-    }
+    }*/
 }
 
 
